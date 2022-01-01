a535f8d64545837b152e8c4e215d31123a48ac35
SimontheSparrow <88796995+SimontheSparrow@users.noreply.github.com>
2022-01-01 15:41:39 +0100

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication4.Models;
+
+namespace WebApplication4.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+       public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ Services/Interfaces/IImageService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Services.Interfaces
+{
+    public interface IImageService
+    {
+        Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+        string ConvertByteArrayToFile(byte[] fileData, string extension);
+    }
+}
